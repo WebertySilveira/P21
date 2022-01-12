9c60cc4542ada5770187ccd67c3a590c0561c842
WebertySilveira <weberty.silveira.sousa@gmail.com>
2022-01-11 23:26:40 -0300

    Integrar Cabeçalho e Rodapé

--- src/views/header.php
+++ src/views/header.php
@@ -1,1 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>AllBlacks Fans</title>
+</head>
+<body>
+    <header class="p-3 bg-dark text-white">
+        <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
 
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="<?php echo(__DIR__ . '/index.php'); ?>" class="nav-link px-2 text-secondary">Home</a></li>
+            </ul>
+
+            <div class="text-end">
+                <a href="#" type="button" class="btn btn-outline-light me-2">Adicionar Torcedor</a>
+                <a href="#" type="button" class="btn btn-warning">Gerar Planilhar</a>
+            </div>
+        </div>
+    </div>
+  </header>
